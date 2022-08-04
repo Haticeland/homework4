399d885d47a0cf639d3011f579a99801da19cc85
Yulia Shelkovenko <95879048+Haticeland@users.noreply.github.com>
2022-08-04 16:54:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ function getColor(num1, num2, num3) {
 const resultColor = getColor(23,100);
 console.log(resultColor);
 console.log('task 5');
-
+//не уверена, что 4 задание выполнено верно
 /* 
     TASK 5:
 
